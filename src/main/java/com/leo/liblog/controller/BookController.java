09964a059be099fb3787ccd503fe90b0888568d0
CHAEYULL <leopportunity5@gmail.com>
2024-03-22 16:54:52 +0900

    list페이지 제작중

--- src/main/java/com/leo/liblog/controller/BookController.java
+++ src/main/java/com/leo/liblog/controller/BookController.java
@@ -2,11 +2,13 @@ package com.leo.liblog.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +36,11 @@ public class BookController {
 	@ResponseBody
 	List<Book> getBookData(){
 		List<Book> result = bookRepository.findAll();
+		return result;
+	}
+	@GetMapping("/postEditData/{id}")
+	Optional<Book> getBookEditData(@PathVariable("id") Long id){
+		Optional<Book> result = bookRepository.findById(id);
 		return result;
 	}
 
